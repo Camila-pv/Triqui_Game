f2026a57d282e99c43d5094d316fe0f43eb650fa
Camila <mariac.patarroyo@urosario.edu.co>
2021-05-14 22:40:16 -0500

    Answer machine

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@
 
 int main() {
   Tablero t;
+  t.instruction();
+  std::cout <<"\n";
   int posicion = posi_random();
   t.assingment(posicion,'X');
   t.display();
   std::cout <<"\n";
-  while(t.translate_won() == 5) {
-    std::cout << t.translate_won();
+
+  while(t.translate_won() == -30) {
     int pos;
     std::cin >> pos;
     t.assingment(pos, 'O');
     t.display();
     t.thinker();
   }
+
+  if(t.translate_won()== 10)
+    std::cout <<"The machine won";
+  else if(t.translate_won()== -10)
+    std::cout <<"The player won";
+  else
+    std::cout <<"Tie !!"<<std::endl;
+  std::cout <<"\n";
+  std::cout <<"\n";
+
   return 0;
 }
