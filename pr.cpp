86f3be8c0501b265f98966db771852955dde7cf9
Camila <mariac.patarroyo@urosario.edu.co>
2021-05-14 22:42:34 -0500

    Answe machine

--- pr.cpp
+++ pr.cpp
@@ -29,18 +29,23 @@ void Tablero::display(){
 }
 
 void Tablero::instruction(){
+  std::cout << " -----------------------------------------------------\n";
+  std::cout << "                       TIC TAC TOE\n";
+  std::cout << " -----------------------------------------------------\n";
+  std::cout << "                 X: Machine     O: Player\n";
+  std::cout << "These are the number of each box\n";
   bool f1e = true;
   std::cout << " -----------\n";
   for(int i = 0; i < 9; i++) {
     if(f1e){
-        std::cout<< "  " << i;
+        std::cout<< "  " << i+1;
         f1e = false;
     }else if((i+1)%3 == 0) {
-      std::cout<< " | "<< i <<"\n";
+      std::cout<< " | "<< i+1 <<"\n";
       std::cout << " -----------\n";
       f1e = true;
     }else{
-      std::cout << " | "<< i;
+      std::cout << " | "<< i+1;
     }
   }
 }
@@ -59,24 +64,6 @@ int posi_random(){
   return 1;
 }
 
-void Tablero::thinker(){
-  int best_position;
-  int best_score = -100;
-  for(int i = 0; i < 9; i++) {
-    if(tablero[i] == '-') {
-      tablero[i] = 'X';
-      int score = minimax(0, false);
-      tablero[i] = '-';
-      if(score > best_score){
-        best_score = score;
-        best_position = i;
-      }
-    }
-  }
-  assingment(best_position, 'X');
-  display();
-}
-
 //Determina si toda la cuadricula esta llena con 'X' o 'O'
 bool Tablero::full(){
   for(int i = 0; i < 9; i++){
@@ -93,7 +80,6 @@ bool Tablero::full(){
 char Tablero::won(){
   //Fila
   for(int i = 0; i < 9 ; i = i+3){
-    //std::cout << "holaaaa" <<i <<'\n';
     if((tablero[i]==tablero[i+1]) && (tablero[i+1]== tablero[i+2]) && (tablero[i]== 'O'))
       return 'O';
     if((tablero[i]==tablero[i+1]) && (tablero[i+1]== tablero[i+2]) && (tablero[i]== 'X'))
@@ -129,45 +115,73 @@ char Tablero::won(){
 int Tablero::translate_won() {
   char plyr = won();
   if(plyr == 'X') {//gana X
-    return 1;
+    return 10;
   }else if(plyr == 'O'){//gana O
-    return -1;
+    return -10;
   }else if(plyr == 'N'){
-    return 5;//no hay ganador aun
+    return -30;//no hay ganador aun
   }else{
-    return 0;//empatados
+    return -25;//empatados
   }
 }
 
-int Tablero::minimax(int deep, bool turn) {
-  std::cout << "hola";
-  int game_state = translate_won();
+int Tablero::minimax(int deep, bool turn){
+  if(translate_won()!=-30){
+    return translate_won();
+  }
   //turno de la maquina ->max
   if(turn) {// esta maximizando
+    int best_position;
     int best_score = -100;
-    for(int i = 0; i < 9; i++) {
-      if(tablero[i] == '-') {
+    for(int i = 0; i < 9; i++){
+      if(tablero[i] == '-'){
         tablero[i] = 'X';
         int score = minimax(deep+1, false);
         tablero[i] = '-';
-        if(score > best_score) {
+        if(score > best_score){
           best_score = score;
+          best_position = i;
         }
       }
     }
-    return best_score;
-  }else {//turno del usuario->min
+    //std::cout<< "machine"<<best_score;
+    return best_position;
+  }else{//turno del usuario->min
     int best_score = 100;
+    int best_position;
     for(int i = 0; i < 9; i++) {
       if(tablero[i] == '-') {
         tablero[i] = 'O';
         int score = minimax(deep+1, true);
         tablero[i] = '-';
-        if(score < best_score) {
+        if(score < best_score){
           best_score = score;
+          best_position = i;
+
         }
       }
     }
-    return best_score;
+    return best_position;
   }
+}
+
+void Tablero::thinker(){
+/**
+  int best_position;
+  int best_score = -100;
+  for(int i = 0; i < 9; i++) {
+    if(tablero[i] == '-') {
+      tablero[i] = 'X';
+      int score = minimax(0, false);
+      tablero[i] = '-';
+      if(score > best_score){
+        best_score = score;
+        best_position = i;
+      }
+    }
+  }
+**/
+  //int best_position = minimax(0, false);
+  assingment(best_position+1, 'X');
+  display();
 }
